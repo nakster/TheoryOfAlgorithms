558da9cb045eba372e556ceab4a3ce12c75e8ec7
Nakster <g00332403@gmit.ie>
2019-02-23 18:11:16 +0000

    main func and sha256 func added

---
+++ sha256/sha256.c
@@ -0,0 +1,17 @@
+// Naqi Ahmad 2019 prject
+// hash sha256 alogrithm 
+// Input and Output operations performed
+#include <stdio.h>
+// provides a set of typedefs that specify exact-width integer types
+#include <stdint.h>
+
+void sha256();
+
+int main(int argc, char *argv[]){
+    
+    return 0;
+}
+
+void sha256(){
+  
+}
