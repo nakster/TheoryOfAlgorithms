f94b6db773dad937ab3faa31eb2c31ffe650e7a2
Nakster <g00332403@gmit.ie>
2019-02-23 18:49:52 +0000

    for loop

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -13,5 +13,33 @@ int main(int argc, char *argv[]){
 }
 
 void sha256(){
+
+    //message schedule
+    uint32_t W[64]; 
+    //working variables section 6.2
+    uint32_t a, b, c, d, e, f, g, h;
+     // two temp variables 
+    uint32_t T1, T2; 
   
+    // the hash value 
+    uint32_t H[8] = {
+         0x6a09e667,
+         0xbb67ae85,
+         0x3c6ef372,
+         0xa54ff53a,
+         0x510e527f,
+         0x9b05688c,
+         0x1f83d9ab,
+         0x5be0cd19
+    };
+  
+    // the current message block
+    uint32_t M[16];
+
+    // for loop
+    for (i = 0,i < 16; ++i) {
+        W[i] = M[i];
+    }
+
+
 }
