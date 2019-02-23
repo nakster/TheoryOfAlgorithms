bf21f8f729c513ebb3bb67a7b415972b84208c4e
Nakster <g00332403@gmit.ie>
2019-02-23 19:03:45 +0000

    sha256 functions

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -5,6 +5,17 @@
 // provides a set of typedefs that specify exact-width integer types
 #include <stdint.h>
 
+//----------Macros-------------
+#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
+#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
+
+#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
+#define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
+#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
+
 void sha256();
 
 int main(int argc, char *argv[]){
@@ -36,10 +47,15 @@ void sha256(){
     // the current message block
     uint32_t M[16];
 
+    int t;
     // for loop
-    for (i = 0,i < 16; ++i) {
-        W[i] = M[i];
+    // from page 22, w[t] = m[t] for 0 <= t 15
+    for (t = 0,t < 16; ++t) {
+        W[t] = M[t];
     }
 
+    for (t=16 ; t < 64; ++t){
+        SIG1(W[t - 2]) + W[t - 7] + SIG0(W[t - 15]) + W[t - 16];
+    }
 
 }
