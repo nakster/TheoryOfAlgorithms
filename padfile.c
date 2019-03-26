7ed0804d34e2144843aa2bce40d35883105d67a1
Nakster <g00332403@gmit.ie>
2019-03-26 17:51:45 +0000

    check file exists

--- padfile.c
+++ padfile.c
@@ -24,53 +24,54 @@ int main(int argc, char *argv[]) {
 
      enum status s = READ;
 
-     f = fopen(argv[1], "r");
+    f = fopen(argv[1], "r");
+    if(NULL != f){
+         while (s == READ) {
+            nobytes = fread(M.e, 1, 64, f);
+            printf("%llu\n", nobytes);
+            nobits = nobits + (nobytes * 8);
 
-     while (s == READ) {
-         nobytes = fread(M.e, 1, 64, f);
-         printf("%llu\n", nobytes);
-         nobits = nobits + (nobytes * 8);
-
-         if(nobytes < 56){
-            printf("I've Found a block with less then 55 bytes\n");
-            M.e[nobytes] = 0x80;
-            while(nobytes < 56){
+            if(nobytes < 56){
+                printf("I've Found a block with less then 55 bytes\n");
+                M.e[nobytes] = 0x80;
                 
-                nobytes = nobytes + 1;
-                M.e[nobytes] = 0x00;
+                while(nobytes < 56){
+                
+                   nobytes = nobytes + 1;
+                   M.e[nobytes] = 0x00;
+                }
+                M.s[7] = nobits;
+                s = FINISH;
+            }else if (nobytes < 64){
+                s = PAD0;
+                M.e[nobytes] = 0X80;
+                while( nobytes < 64){
+                    nobytes = nobytes + 1;
+                    M.e[nobytes] = 0x00;
+                }
+            }else if(feof(f)){
+                s = PAD1;
             }
-
-            M.s[7] = nobits;
-            s = FINISH;
-         }else if (nobytes < 64){
-            s = PAD0;
-            M.e[nobytes] = 0X80;
-            while( nobytes < 64){
-                nobytes = nobytes + 1;
-                M.e[nobytes] = 0x00;
-            }
-         }else if(feof(f)){
-             s = PAD1;
-         }
          
-        // printf("%llu\n", nobytes);
-     } // end loop
+                 // printf("%llu\n", nobytes);
+         } // end loop 
+        if(s == PAD0 || s == PAD1){
+             for(i =0; i<56; i++){
+                 M.e[i] = 0x00;
+             }
+             M.s[7] = nobits;
+         }
+         if(s == PAD1){
+             M.e[0] = 0x80;
+         }
+         fclose(f);
 
-     
-     if(s == PAD0 || s == PAD1){
-        for(i =0; i<56; i++){
-            M.e[i] = 0x00;
+        for(int i= 0; i < 64; i++)
+             printf("%x ", M.e[i]);
+             printf("\n");
+        }else{
+            puts("File Doesnt open!!!");
         }
-        M.s[7] = nobits;
-     }
-     if(s == PAD1){
-        M.e[0] = 0x80;
-     }
-     fclose(f);
-
-     for(int i= 0; i < 64; i++)
-         printf("%x ", M.e[i]);
-         printf("\n");
-
+    
      return 0;
 } // end main
