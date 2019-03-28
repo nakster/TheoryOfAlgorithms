162c44fa98f17b826f77edc97c6324fb002b3ab0
NAQI AHMAD - Student <G00332403@gmit.ie>
2019-03-28 15:05:27 +0000

    added the padding

--- sha256/sha256.c
+++ sha256/sha256.c
@@ -139,6 +139,65 @@ void sha256(){
 
  }//end of sha256 methid
 
+ 
+ int msgblockmethod(int argc, char *argv[]) {
+	union msgblock M;
+	uint64_t nobytes;
+	uint64_t nobits = 0;
+	FILE *f;
+	int i =0;
+
+	enum status s = READ;
+
+	f = fopen(argv[1], "r");
+
+	while (s == READ) {
+		nobytes = fread(M.e, 1, 64, f);
+		printf("%llu\n", nobytes);
+		nobits = nobits + (nobytes * 8);
+
+		if(nobytes < 56){
+			printf("I've Found a block with less then 55 bytes\n");
+			M.e[nobytes] = 0x80;
+			while(nobytes < 56){
+				
+				nobytes = nobytes + 1;
+				M.e[nobytes] = 0x00;
+			}
+
+			M.s[7] = nobits;
+			s = FINISH;
+		}else if (nobytes < 64){
+			s = PAD0;
+			M.e[nobytes] = 0X80;
+			while( nobytes < 64){
+				nobytes = nobytes + 1;
+				M.e[nobytes] = 0x00;
+			}
+		}else if(feof(f)){
+			 s = PAD1;
+		}
+	} // end loop
+
+	 
+	if(s == PAD0 || s == PAD1){
+		for(i =0; i<56; i++){
+			M.e[i] = 0x00;
+		}
+		M.s[7] = nobits;
+	}
+	if(s == PAD1){
+		M.e[0] = 0x80;
+	}
+	
+	fclose(f);
+
+	for(int i= 0; i < 64; i++)
+		printf("%x ", M.e[i]);
+		printf("\n");
+
+	return 0;
+} // end main
 
 
 
